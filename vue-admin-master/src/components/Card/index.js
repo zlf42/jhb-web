ef5ae9c5dbc7ddc4d782380696ecf9a9b99fd4bf
zlf42 <13849920672@163.com>
2020-05-25 14:24:42 +0800

    no message

--- vue-admin-master/src/components/Card/index.js
+++
@@ -1,4 +0,0 @@
-export const MtCard = () => import('./card');
-export const MtCardHeader = () => import('./card_header');
-export const MtCardBody = () => import('./card_body');
-export const MtCardFooter = () => import('./card_footer');
--- vue-admin-master/src/store/state.js
+++
@@ -1,6 +0,0 @@
-const state = {
-  user: null,
-  token: ''
-};
-
-export default state;
